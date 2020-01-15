7d4eaeb352ca53b1a0be4cd5109f5564745fc976
Quentin Michel <quentinmichel69110@gmail.com>
2020-01-15 08:21:59 +0100

    fix doEchoAfter

--- docs/src.e31bb0bc.js
+++ docs/src.e31bb0bc.js
@@ -38250,7 +38250,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36337" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "32795" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
